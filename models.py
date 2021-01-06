81ce5e09b3c78ee8e08d1c4dd6a908c1956500ce
Aliaksandr Spichakou <aspichakou@eisgroup.com>
2021-01-06 20:59:02 +0300

    Left only one model

--- models.py
+++ models.py
@@ -5,10 +5,8 @@ from modules.tts.synthesizer import Synthesizer
 config = Synthesizer.load_config("data/config.yaml")
 
 natasha = Synthesizer.from_config(config, name="natasha")
-ruslan = Synthesizer.from_config(config, name="ruslan")
 
 models = {
     "Natasha": natasha,
-    "Ruslan": ruslan,
     Config.ALL_MODELS_KEY: None
 }
